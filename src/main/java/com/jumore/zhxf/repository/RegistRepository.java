ebe937f801ac6a950bc94d781cefb950ab74f161
rabbit <fans_2046@126.com>
2017-07-16 11:22:27 +0800

    mvn install ok

--- src/main/java/com/jumore/zhxf/repository/RegistRepository.java
+++ src/main/java/com/jumore/zhxf/repository/RegistRepository.java
@@ -1,14 +1,12 @@
 package com.jumore.zhxf.repository;
 
 import com.jumore.zhxf.domain.Regist;
-import com.jumore.zhxf.domain.oa.CheckConfig;
+
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the Regist entity.
@@ -16,6 +14,6 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface RegistRepository extends JpaRepository<Regist,Long> {
 	Page<Regist> findByEventId (Long eventId,Pageable pageable);
-	
+
 	long countByUserIdEqualsAndEventIdEquals(Long UserId ,Long EventId);
 }
